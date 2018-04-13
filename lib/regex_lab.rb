246b579febe2e306f04f719ddafaf1a47a80aa94
Ji Yim <gsooyim@gmail.com>
2018-04-13 01:17:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,11 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].+\W$/)
-    true
-  else
-    false
-  end
+  text.match(/^[A-Z].+\W$/) ? true : false
 end
 
 def valid_phone_number?(phone)
